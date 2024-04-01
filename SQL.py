25122007d85ae1ecff076e827b02f84f089125fc
whatsupjax <160513346+whatsupjax@users.noreply.github.com>
2024-03-31 22:52:07 -0500

    1

--- SQL.py
+++
@@ -1,9 +0,0 @@
-import sqlite3 as sql
-
-connection = sql.connect('User.db')
-cursor = connection.cursor()
-result = cursor.fetchall()
-
-connection.execute('''CREATE TABLE IF NOT EXISTS Users (Username VARCHAR(20), Password VARCHAR(20))''')
-
-connection.execute('''INSERT INTO Users''')
